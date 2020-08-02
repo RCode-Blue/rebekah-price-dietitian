003d0425a20fb43e80000a2452bf20be3306d013
Ricky <rickyliew@hotmail.com>
2020-08-03 00:15:09 +1000

    deployment #5

--- src/pages/services.js
+++ src/pages/services.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import Layout from "../components/Layout";
 import Navbar from "../components/Navbar";
-import Disorders from "../components/services_/Disorders";
-import Programs from "../components/services_/Programs";
+import Disorders from "../components/services/Disorders";
+import Programs from "../components/services/Programs";
 import Footer from "../components/footer/Footer";
 import Clinics from "../components/Clinics";
 import Attribution from "../components/footer/Attribution";
